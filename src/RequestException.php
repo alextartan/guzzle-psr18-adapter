743501a954eee23310475ebcea9c0b3188187554
Alex Tartan <alex.tartan@gmail.com>
2018-11-14 14:06:40 +0200

    exception cleanup

--- src/RequestException.php
+++ src/RequestException.php
@@ -3,43 +3,8 @@ declare(strict_types=1);
 
 namespace AlexTartan\GuzzlePsr18Adapter;
 
-use Exception;
 use Psr\Http\Client\RequestExceptionInterface;
-use Psr\Http\Message\RequestInterface;
-use Throwable;
 
-final class RequestException extends Exception implements RequestExceptionInterface
+final class RequestException extends BasePsr18Exception implements RequestExceptionInterface
 {
-    /** @ var RequestInterface */
-    private $request;
-
-    /**
-     * PHP>7.2 allows overriding a public constructor with a private one,
-     * but prior to this version, it throws a fatal error.
-     *
-     * Marking as internal to force passing the RequestInterface parameter as required by PSR-18
-     *
-     * @internal
-     */
-    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
-    public static function fromRequest(
-        RequestInterface $request,
-        $message = '',
-        $code = 0,
-        Throwable $previous = null
-    ): self {
-        $exception          = new self($message, $code, $previous);
-        $exception->request = $request;
-
-        return $exception;
-    }
-
-    public function getRequest(): RequestInterface
-    {
-        return $this->request;
-    }
 }
